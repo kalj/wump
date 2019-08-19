b07216fbb934ce59d18576d22a01a20adda36af8
Karl Ljungkvist <k.ljungkvist@gmail.com>
2019-08-19 21:47:46 +0200

    print time and use new lcd api

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 
 mod lcd;
+extern crate chrono;
+
+use chrono::{Local, DateTime};
 
 use lcd::Lcd;
 use std::time::Duration;
@@ -16,54 +19,16 @@ fn main() {
     lcd.init();
 
     // Send some test
-    lcd.print_string("Raspberry Pi", lcd::LINE_1);
-    lcd.print_string("16x2 LCD Test", lcd::LINE_2);
+    lcd.set_lines("Alarm Clock 0.3","Starting up...");
 
-    // 3 second delay
-    sleep(Duration::new(3,0));
-    // let mut i = 0;
-    // while i < 256 {
-    //     let line1 : Vec<u8> = (0..16).map(|x| (x+i) as u8).collect();
-    //     let line2 : Vec<u8> = (16..32).map(|x| (x+i) as u8).collect();
-    //     lcd.send_bytestr(&line1, lcd::LINE_1);
-    //     lcd.send_bytestr(&line2, lcd::LINE_2);
-    //     i += 32;
-
-    //     println!("currently displaying:");
-    //     for d in line1 {
-    //         print!("{} ",d);
-    //     }
-    //     println!();
-    //     for d in line2 {
-    //         print!("{} ",d);
-    //     }
-    //     println!();
-    //     sleep(Duration::new(1,0));
-    // }
+    // 1 second delay
+    sleep(Duration::new(1,0));
 
     loop {
-        let mut input = String::new();
-        print!("write something to display:");
-        let _=io::stdout().flush();
-        io::stdin().read_line(&mut input).expect("Failed reading input");
-        let mut line1 = input.trim();
+        let now : DateTime<Local> = Local::now();
 
-        if line1.len() == 0 {
-            break;
-        }
-
-        let mut line2= "";
-        if line1.len() > lcd::WIDTH {
-            line2 = &line1[lcd::WIDTH..];
-            line1 = &line1[..lcd::WIDTH];
-        }
-
-        lcd.print_string(line1,lcd::LINE_1);
-        lcd.print_string(line2,lcd::LINE_2);
+        lcd.set_lines(&now.format("     %H:%M      ").to_string(),"");
 
         sleep(Duration::new(1,0));
     }
-
-    lcd.print_string("Bye!",lcd::LINE_1);
-    lcd.print_string("",lcd::LINE_2);
 }
